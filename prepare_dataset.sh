3280fb0bd4cccc51a307eff2213b08a8df5f6b17
ZhongyiLin <moderato@ZhongyiLindeMacBook-Pro.local>
2018-03-03 15:54:15 -0800

    Minor fix

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -1,10 +1,11 @@
+cd $DATASET_ROOT
+
 # Get GTSRB dataset
 wget http://benchmark.ini.rub.de/Dataset/GTSRB_Final_Training_Images.zip
 wget http://benchmark.ini.rub.de/Dataset/GTSRB_Final_Test_Images.zip
 wget http://benchmark.ini.rub.de/Dataset/GTSRB_Final_Test_GT.zip
 
 # Unzip dataset
-cd $DATASET_ROOT
 mkdir GTSRB
 tar -xf GTSRB_Final_Training_Images.zip
 tar -xf GTSRB_Final_Test_Images.zip
